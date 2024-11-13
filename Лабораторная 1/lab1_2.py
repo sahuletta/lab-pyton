42e695b2ff509dc99d4fd8a9729acadfbdbcb2bf
sahuletta <sashayakubova54@gmail.com>
2024-11-13 21:03:45 +0300

    Update lab1_2.py

--- Лабораторная 1/lab1_2.py
+++ Лабораторная 1/lab1_2.py
@@ -6,5 +6,5 @@ number_of_symbols = 25
 
 storage_in_bytes = storage * 1024 * 1024
 book_size = number_of_symbols * number_of_lines * number_of_pages * 4
-max_books = round(storage_in_bytes / book_size)
+max_books = storage_in_bytes // book_size
 print("Количество книг, помещающихся на дискету:", max_books)
